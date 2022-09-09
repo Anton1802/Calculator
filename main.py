44730f8c9193cec6eb6f6912e0264a117007f9a3
Anton1802 <anton.sup1802@gmail.com>
2022-09-09 04:09:51 +0300

    Add basic functions

--- main.py
+++ main.py
@@ -2,10 +2,61 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.config import Config
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', 400)
+Config.set('graphics', 'height', 500)
 
 
 class Calculator(Widget):
-    pass
+
+    formula = ''
+    result = 0
+
+    def formula_add(self, text, visual=''):
+        if visual != '':
+            self.input.text += visual
+        else:
+            self.input.text += text
+        self.formula += text
+
+    def start_operation(self):
+        try:
+            self.result = eval(self.formula)
+        except SyntaxError:
+            self.input.text = "Error"
+
+        if self.result != 0:
+            self.input.text = str(self.result)
+            self.formula = str(self.result)
+        else:
+            self.input.text = "Error"
+
+    def reset(self):
+        self.input.text = ''
+        self.result = 0
+        self.formula = ''
+
+    def clear(self):
+        str_list = []
+        str = self.input.text
+        str_list = list(str)
+        formula_list = []
+        formula = self.formula
+        formula_list = list(formula)
+
+        for symbol in str_list:
+            if symbol == '%':
+                self.input.text = self.input.text.translate
+                ({ord(i): None for i in '/100'})
+                self.formula = self.formula.translate
+                ({ord(i): None for i in '/100'})
+        try:
+            self.input.text = self.input.text.replace(str_list.pop(), '', 1)
+            self.formula = self.formula.replace(formula_list.pop(), '', 1)
+        except IndexError:
+            self.input.text = ""
+            self.formula = ""
 
 
 class CalculatorApp(App):
